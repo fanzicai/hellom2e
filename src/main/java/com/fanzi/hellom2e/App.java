89dba8715587bf893148672377b01c046a4c3cda
caizf <fanzicai@yahoo.com>
2016-06-17 11:29:19 +0800

    files

---
+++ src/main/java/com/fanzi/hellom2e/App.java
@@ -0,0 +1,13 @@
+package com.fanzi.hellom2e;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
